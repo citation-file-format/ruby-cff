32415e7d8cebbb1dcfe3e9764fda88ee236a373f
Robert Haines <rhaines@manchester.ac.uk>
2021-07-23 19:58:14 +0100

    Add an `Identifier` type.

---
+++ lib/cff/identifier.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2018-2021 The Ruby Citation File Format Developers.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##
+module CFF
+
+  # An Identifier represents an identifier in a CITATION.cff file.
+  #
+  # Identifier implements all of the fields listed in the
+  # [CFF standard](https://citation-file-format.github.io/). All fields
+  # are simple strings and can be set as such. A field which has not been set
+  # will return the empty string. The simple fields are (with defaults in
+  # parentheses):
+  #
+  # * `type`
+  # * `value`
+  class Identifier < ModelPart
+
+    ALLOWED_FIELDS = ['type', 'value'].freeze # :nodoc:
+
+    # The [defined set of identifier types](https://github.com/citation-file-format/citation-file-format/blob/main/README.md#identifier-type-strings).
+    IDENTIFIER_TYPES = ['doi', 'url', 'swh', 'other'].freeze
+
+    # :call-seq:
+    #   new -> Identifier
+    #   new { |id| block } -> Identifier
+    #   new(type, value) -> Identifier
+    #   new(type, value) { |id| block } -> Identifier
+    #
+    # Create a new Identifier with the optionally supplied type and value.
+    # If the supplied type is invalid, then neither the type or value are set.
+    def initialize(param = nil, *more)
+      if param.is_a?(Hash)
+        @fields = param
+      else
+        @fields = Hash.new('')
+
+        unless param.nil?
+          self.type = param
+          @fields['value'] = more[0] unless @fields['type'].empty?
+        end
+      end
+
+      yield self if block_given?
+    end
+
+    # :call-seq:
+    #   type = type
+    #
+    # Sets the type of this Identifier. The type is restricted to a
+    # [defined set of identifier types](https://github.com/citation-file-format/citation-file-format/blob/main/README.md#identifier-type-strings).
+    def type=(type)
+      type = type.downcase
+      @fields['type'] = type if IDENTIFIER_TYPES.include?(type)
+    end
+  end
+end
---
+++ test/cff_identifier_test.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2018-2021 The Ruby Citation File Format Developers.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'test_helper'
+
+class CFFIdentifierTest < Minitest::Test
+
+  def test_new
+    id = ::CFF::Identifier.new('doi', '10.5281/zenodo.1184077')
+    assert_equal('doi', id.type)
+    assert_equal('10.5281/zenodo.1184077', id.value)
+  end
+
+  def test_new_no_params
+    id = ::CFF::Identifier.new
+    assert_empty(id.type)
+    assert_empty(id.value)
+  end
+
+  def test_new_bad_type
+    id = ::CFF::Identifier.new('xxx', '0123456789')
+    assert_empty(id.type)
+    assert_empty(id.value)
+  end
+
+  def test_bad_methods_not_allowed
+    id = ::CFF::Identifier.new
+
+    refute_respond_to(id, :aaaaaaa)
+    assert_raises(NoMethodError) do
+      id.aaaaaaa
+    end
+
+    refute_respond_to(id, :Type)
+    assert_raises(NoMethodError) do
+      id.Type = 'swh'
+    end
+  end
+
+  def test_type_restricted_to_allowed_types
+    id = ::CFF::Identifier.new
+
+    id.type = 'doi'
+    assert_equal('doi', id.type)
+
+    id.type = 'xxx'
+    assert_equal('doi', id.type)
+
+    id.type = 'SWH'
+    assert_equal('swh', id.type)
+  end
+
+  def test_simple_fields_set_and_output_correctly
+    id = ::CFF::Identifier.new
+    value = 'swh:1:rel:99f6850374dc6597af01bd0ee1d3fc0699301b9f'
+
+    assert_equal('', id.value)
+    id.value = value
+    assert_equal(value, id.value)
+
+    y = id.fields.to_yaml
+
+    assert_includes(y, "value: #{value}\n")
+  end
+end
