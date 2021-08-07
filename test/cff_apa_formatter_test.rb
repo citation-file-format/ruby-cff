5cdcf418726ba4736f10881b1052ef6e23bd1885
Robert Haines <rhaines@manchester.ac.uk>
2021-08-07 17:43:10 +0100

    Streamline formatter tests.
    Better detect that a bad input will return `nil` from a formatter.

--- test/cff_apa_formatter_test.rb
+++ test/cff_apa_formatter_test.rb
@@ -4,15 +4,17 @@ require 'test_helper'
 
 class CFFApaFormatterTest < Minitest::Test
 
-  include ::CFF::Util
-
   describe 'all apa fixtures' do
     Dir[::File.join(FILES_DIR, '*.cff')].each do |input_file|
       define_method("test_converter_for_#{File.basename(input_file)}") do
         cff = ::CFF::File.read(input_file)
         output_file = ::File.join(CONVERTED_DIR, "#{File.basename(input_file, '.*')}.apa")
 
-        assert_equal File.read(output_file).strip!, cff.to_apalike
+        if ::File.exist?(output_file)
+          assert_equal File.read(output_file).strip, cff.to_apalike
+        else
+          assert_nil cff.to_apalike
+        end
       end
     end
   end
--- test/cff_bibtex_formatter_test.rb
+++ test/cff_bibtex_formatter_test.rb
@@ -4,15 +4,17 @@ require 'test_helper'
 
 class CFFBibtexFormatterTest < Minitest::Test
 
-  include ::CFF::Util
-
   describe 'all bibtex fixtures' do
     Dir[::File.join(FILES_DIR, '*.cff')].each do |input_file|
       define_method("test_converter_for_#{File.basename(input_file)}") do
         cff = ::CFF::File.read(input_file)
         output_file = ::File.join(CONVERTED_DIR, "#{File.basename(input_file, '.*')}.bibtex")
 
-        assert_equal File.read(output_file).strip!, cff.to_bibtex
+        if ::File.exist?(output_file)
+          assert_equal File.read(output_file).strip, cff.to_bibtex
+        else
+          assert_nil cff.to_bibtex
+        end
       end
     end
   end
