63f0f49e24127f071c94aa134bb55ba30e89ffef
Robert Haines <rhaines@manchester.ac.uk>
2022-10-08 10:45:13 +0100

    Move formatter tests into a subdirectory.

--- test/formatters/cff_formatter_test.rb
+++ test/formatters/cff_formatter_test.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require_relative 'test_helper'
+require_relative '../test_helper'
 
 require 'cff/formatters/formatter'
 require 'cff/index'
